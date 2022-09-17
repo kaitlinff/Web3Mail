edb4a2502056a692b83d5a703294cddd2a01fd75
kaitlinffsil <kaitlinffsil@gmail.com>
2022-09-17 19:37:44 +0800

    add config.js

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const NFT_STORAGE_APIKEY = "<KEY>";
+export const NFTPORT_API = "<KEY>";
+export const POAP_TOKEN = "<KEY>";
+export const POAP_APIKEY = "<KEY>";
+export const UNSTOPPABLEDOMAINS_CLIENTID = "<KEY>";
+export const UNSTOPPABLEDOMAINS_REDIRECT_URI = "<KEY>";
+export const ALCHEMYAPI_KEY = "<KEY>";
